d6748c16c44376537657316a570c91e205c5283b
urker <uros_radic@yahoo.com>
2018-08-24 20:47:29 +0100

    final challenge for node server

--- server.js
+++ server.js
@@ -44,7 +44,11 @@ app.get('/', (req, res) => {
   });
 });
 
-
+app.get('/project', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Project Page'
+  });
+});
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
